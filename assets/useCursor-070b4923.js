c8d6ede599ebe4469f90272bbdb4ab39cf2aa550
Santista <terra_alliance@proton.me>
2024-04-19 00:42:23 -0300

    Updates

---
+++ assets/useCursor-070b4923.js
@@ -0,0 +1,1 @@
+import{r as u}from"./index-dea7e4a9.js";function c(r,t="pointer",s="auto",o=document.body){u.useEffect(()=>{if(r)return o.style.cursor=t,()=>void(o.style.cursor=s)},[r])}export{c as u};
