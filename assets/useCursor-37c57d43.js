28c6158978bc2944d7efb755dd78a22708ae1d43
Santista <terra_alliance@proton.me>
2024-04-19 00:50:01 -0300

    Updates

---
+++ assets/useCursor-37c57d43.js
@@ -0,0 +1,1 @@
+import{r as u}from"./index-0bf69bcd.js";function c(r,t="pointer",s="auto",o=document.body){u.useEffect(()=>{if(r)return o.style.cursor=t,()=>void(o.style.cursor=s)},[r])}export{c as u};
--- assets/useCursor-5158b90c.js
+++
@@ -1,1 +0,0 @@
-import{r as u}from"./index-7fd2e730.js";function c(r,t="pointer",s="auto",o=document.body){u.useEffect(()=>{if(r)return o.style.cursor=t,()=>void(o.style.cursor=s)},[r])}export{c as u};
